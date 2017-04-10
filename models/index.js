ae9f3ded12c1ceab85c0bdbc596bc50a0016a9cc
AlexiOHearn <alexiohearn@o.local>
2017-04-10 15:42:19 -0600

    initial commit

--- models/index.js
+++ models/index.js
@@ -3,6 +3,6 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/personal-api");
+                  "mongodb://localhost/personal-api"); 
 
 // module.exports.Campsite = require("./campsite.js.example");
